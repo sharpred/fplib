59b16ef6250cfc6c9a91c98a92595e5a7d2a333e
Paul Ryan <paul@sharpred.com>
2015-11-11 08:33:42 +0000

    grunt

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        jshint : {
+            all : ['Gruntfile.js', 'libs/*.js'],
+            options : {
+                jshintrc : '.jshintrc'
+            }
+        },
+
+        // Before generating any new files, remove any previously-created files.
+        clean : {
+            tests : ['tmp']
+        }
+
+    });
+
+    // Actually load this plugin's task(s).
+    grunt.loadTasks('tasks');
+
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+    // By default, lint and run all tests.
+    grunt.registerTask('default', ['jshint']);
+
+};
