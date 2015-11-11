3ac479674dff6178204015899d9b9b34367b307c
Paul Ryan <paul@sharpred.com>
2015-11-11 12:25:20 +0000

    single exported file

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,7 @@
-var chapterone = require("../src/chapterone");
-var chaptertwo = require("../src/chaptertwo");
-var _ = require("underscore");
-var exports = _.extendOwn({}, chapterone);
+var _ = require("underscore"),
+    chapterone = require("../src/chapterone"),
+    chaptertwo = require("../src/chaptertwo"),
+    exports = {};
+_.extendOwn(exports, chapterone);
 _.extendOwn(exports, chaptertwo);
 module.exports = exports;
