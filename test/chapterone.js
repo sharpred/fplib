091a6b423e6d2c6ff6d889a76aac0eb29ff3945e
Paul Ryan <paul@sharpred.com>
2015-11-11 08:19:23 +0000

    changed test syntax

--- test/chapterone.js
+++ test/chapterone.js
@@ -11,22 +11,23 @@ describe("chapter one", function() {
     describe("existy function", function() {
         var existy = chapterOneLibs.existy;
         it("existy should evaluate correctly", function() {
-            expect(existy(null)).to.equal(false);
-            expect(existy(undefined)).to.equal(false);
-            expect(existy( {}.notHere)).to.equal(false);
-            expect(existy(0)).to.equal(true);
-            expect(existy(false)).to.equal(true);
+            expect(existy(null)).to.be.false;
+            expect(existy(undefined)).to.be.false;
+            expect(existy( {}.notHere)).to.be.false;
+            expect(existy(0)).to.be.true;
+            expect(existy(false)).to.be.true;
         });
     });
 
     describe("truthy function", function() {
         var truthy = chapterOneLibs.truthy;
         it("truthy should evaluate correctly", function() {
-            expect(truthy(null)).to.equal(false);
-            expect(truthy(undefined)).to.equal(false);
-            expect(truthy( {}.notHere)).to.equal(false);
-            expect(truthy(0)).to.equal(true);
-            expect(truthy(false)).to.equal(false);
+            expect(truthy(null)).to.be.false;
+            expect(truthy(undefined)).to.be.false;
+            expect(truthy( {}.notHere)).to.be.false;
+            expect(truthy(0)).to.be.true;
+            expect(truthy({})).to.be.true;
+            expect(truthy(false)).to.be.false;
         });
     });
 
@@ -44,9 +45,9 @@ describe("chapter one", function() {
             result1 = doWhen(test1 === true, func);
             result2 = doWhen(test2 === true, func);
             result3 = doWhen(test1 === true, test2);
-            expect(result1).to.equal(true);
-            expect(result2).to.equal(undefined);
-            expect(result3).to.equal(undefined);
+            expect(result1).to.be.true;
+            expect(result2).to.be.undefined;
+            expect(result3).to.be.undefined;
         });
     });
 });
